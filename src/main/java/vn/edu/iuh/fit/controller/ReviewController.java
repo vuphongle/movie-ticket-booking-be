2786bd8b4e957b26053edaf60711cb5d2bb4fa93
LE VU PHONG <lephong29092909@gmail.com>
2025-09-16 20:50:04 +0700

    feat: api for cinema and review movie

--- src/main/java/vn/edu/iuh/fit/controller/ReviewController.java
+++ src/main/java/vn/edu/iuh/fit/controller/ReviewController.java
@@ -1,26 +1,37 @@
 package vn.edu.iuh.fit.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import vn.edu.iuh.fit.model.request.UpsertReviewRequest;
 import vn.edu.iuh.fit.service.MovieService;
 import vn.edu.iuh.fit.service.ReviewService;
 
 @Slf4j
 @RestController
-@RequestMapping("api/public/reviews")
+@RequestMapping("api")
 @RequiredArgsConstructor
 public class ReviewController {
     private final ReviewService reviewService;
     private final MovieService movieService;
 
-    @GetMapping()
+    @GetMapping("/public/reviews")
     public ResponseEntity<?> getAllReviewsOfMovies(@RequestParam(required = false, defaultValue = "1") Integer page,
                                                    @RequestParam(required = false, defaultValue = "6") Integer limit) {
         return ResponseEntity.ok(movieService.getAllReviewsOfMovies(page, limit));
+    }
+
+    @PutMapping("/admin/reviews/{id}")
+    public ResponseEntity<?> adminUpdateReview(@Valid @RequestBody UpsertReviewRequest request,
+                                               @PathVariable Integer id) {
+        return ResponseEntity.ok(reviewService.adminUpdateReview(request, id));
+    }
+
+    @DeleteMapping("/admin/reviews/{id}")
+    public ResponseEntity<?> adminDeleteReview(@PathVariable Integer id) {
+        reviewService.adminDeleteReview(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/vn/edu/iuh/fit/service/ReviewService.java
+++ src/main/java/vn/edu/iuh/fit/service/ReviewService.java
@@ -7,10 +7,16 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import vn.edu.iuh.fit.entity.Movie;
 import vn.edu.iuh.fit.entity.Review;
+import vn.edu.iuh.fit.exception.BadRequestException;
 import vn.edu.iuh.fit.exception.ResourceNotFoundException;
+import vn.edu.iuh.fit.model.request.UpsertReviewRequest;
 import vn.edu.iuh.fit.repository.MovieRepository;
 import vn.edu.iuh.fit.repository.ReviewRepository;
+
+import java.util.List;
 
 @Slf4j
 @Service
@@ -25,5 +31,48 @@ public class ReviewService {
         }
         Pageable pageable = PageRequest.of(page - 1, limit, Sort.by("createdAt").descending());
         return reviewRepository.findByMovie_Id(movieId, pageable);
+    }
+
+    private void updateRatingOfMovie(Movie movie) {
+        List<Review> reviews = reviewRepository.findByMovie_Id(movie.getId());
+        double rating = reviews.stream().mapToDouble(Review::getRating).average().orElse(0);
+        rating = Math.round(rating * 10) / 10.0;
+        movie.setRating(rating);
+        movieRepository.save(movie);
+    }
+
+    @Transactional
+    public Review adminUpdateReview(UpsertReviewRequest request, Integer id) {
+        Movie movie = movieRepository.findById(request.getMovieId())
+                .orElseThrow(() -> new ResourceNotFoundException("Không tìm thấy phim có id = " + request.getMovieId()));
+
+        Review review = reviewRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Không tìm thấy review có id = " + id));
+
+        // check review is for movie
+        if (!review.getMovie().getId().equals(movie.getId())) {
+            throw new BadRequestException("Review không thuộc phim này");
+        }
+
+        review.setComment(request.getComment());
+        review.setRating(request.getRating());
+        review.setMovie(movie);
+
+        reviewRepository.save(review);
+
+        // update rating of movie
+        updateRatingOfMovie(movie);
+
+        return review;
+    }
+
+    @Transactional
+    public void adminDeleteReview(Integer id) {
+        Review review = reviewRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Không tìm thấy review có id = " + id));
+        reviewRepository.delete(review);
+
+        // update rating of movie
+        updateRatingOfMovie(review.getMovie());
     }
 }
