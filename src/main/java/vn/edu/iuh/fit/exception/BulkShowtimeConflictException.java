65a0f426a6808ae5e3921dd4ed6dfc092403b92b
LE VU PHONG <lephong29092909@gmail.com>
2025-09-23 02:19:52 +0700

    showtime-bulk-create

---
+++ src/main/java/vn/edu/iuh/fit/exception/BulkShowtimeConflictException.java
@@ -0,0 +1,14 @@
+package vn.edu.iuh.fit.exception;
+
+import lombok.Getter;
+import vn.edu.iuh.fit.model.response.BulkShowtimeResponse;
+
+@Getter
+public class BulkShowtimeConflictException extends RuntimeException {
+    private final BulkShowtimeResponse conflictDetails;
+
+    public BulkShowtimeConflictException(String message, BulkShowtimeResponse conflictDetails) {
+        super(message);
+        this.conflictDetails = conflictDetails;
+    }
+}
