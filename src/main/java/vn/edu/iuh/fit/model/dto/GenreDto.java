b7aeec6e6d83f4d3cc722cdea830c2dd69c16398
MaiChienNo <nomai6789@gmail.com>
2025-11-17 21:34:41 +0700

    feat: create genreDto

--- src/main/java/vn/edu/iuh/fit/model/dto/GenreDto.java
+++ src/main/java/vn/edu/iuh/fit/model/dto/GenreDto.java
@@ -1,19 +1,17 @@
 package vn.edu.iuh.fit.model.dto;
 
-import jakarta.persistence.Column;
+import java.util.Date;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.Date;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class GenreDto {
-    private Integer id;
-    private String name;
-    private String slug;
-    private Date createdAt;
-    private Date updatedAt;
+  private Integer id;
+  private String name;
+  private String slug;
+  private Date createdAt;
+  private Date updatedAt;
 }
